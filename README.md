fa78c60f08f8e376d2498aa4cad751d31fa21b45
kevin <kevin.vandenbreemen@gmail.com>
2023-11-10 14:19:05 -0800

    Documentation, provide fatjar

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # kcoder_wiki
 Wiki based on the ktt wiki I'm developing
+
+# Building
+You can use the ```gradlew fatjar``` gradle task to build the project into an executable jar
+
+# Plugins
+## umlGen
+Creates UML using the grucd (grand unified code documentor) library.  Example usages:
+
+```
+{@umlGen path=/path/to/code}
+```
+
+This will render as UML for any supported code at the given path.
--- build.gradle.kts
+++ build.gradle.kts
@@ -31,3 +31,25 @@ tasks.test {
 kotlin {
     jvmToolchain(11)
 }
+
+val fatJar = task("FatJar", type = Jar::class) {
+
+    val jarName = "kcoder_wiki.jar"
+
+    archiveFileName.set(jarName)
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    manifest {
+        attributes["Main-Class"] = "com.vandenbreemen.kcoderwiki.Main"
+
+    }
+    from(configurations.runtimeClasspath.get().map {
+        if(it.isDirectory) it else zipTree(it)
+    })
+    with(tasks.jar.get() as CopySpec)
+
+    copy {
+        from("./build/libs/$jarName")
+        into("./")
+    }
+    println("Built and copied $jarName")
+}
