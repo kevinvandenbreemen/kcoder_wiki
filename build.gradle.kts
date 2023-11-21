5dbd6ebc40116bb07cd441278fafba95255b7895
kevin <kevin.vandenbreemen@gmail.com>
2023-11-21 14:30:06 -0800

    Make it possible to publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     kotlin("jvm") version "1.8.21"
+    application
+    java
+    `maven-publish`
+    `java-library`
 }
 
 group = "com.vandenbreemen.kcoderwiki"
@@ -58,4 +62,19 @@ val fatJar = task("FatJar", type = Jar::class) {
         into("./")
     }
     println("Built and copied $jarName")
+}
+
+//  Based on https://github.com/gradle/kotlin-dsl-samples/blob/master/samples/maven-publish/build.gradle.kts
+val sourcesJar by tasks.registering(Jar::class) {
+    classifier = "sources"
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            artifact(sourcesJar.get())
+        }
+    }
 }
