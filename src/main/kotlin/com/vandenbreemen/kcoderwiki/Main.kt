a36140771c0fb0cda3e5c01951cd46df972cd0e8
kevin <kevin.vandenbreemen@gmail.com>
2023-11-21 14:32:26 -0800

    Provide a way to register macros from outside of this program and start it via something other than main()

--- src/main/kotlin/com/vandenbreemen/kcoderwiki/Main.kt
+++ src/main/kotlin/com/vandenbreemen/kcoderwiki/Main.kt
@@ -2,9 +2,17 @@ package com.vandenbreemen.kcoderwiki
 
 import com.vandenbreemen.kcoderwiki.macro.UmlGeneratorMacro
 import com.vandenbreemen.ktt.interactor.StaticContentInteractor
+import com.vandenbreemen.ktt.macro.Macro
 import com.vandenbreemen.ktt.main.WikiApplication
 
 object Main {
+
+    /**
+     * Pre-register a macro before calling startup
+     */
+    fun registerMacro(macro: Macro) {
+        WikiApplication.macroRegistry.register(macro)
+    }
 
     fun setup() {
 
@@ -12,10 +20,17 @@ object Main {
 
     }
 
+    /**
+     * Start the main wiki
+     */
+    fun startup() {
+        WikiApplication.startup()
+    }
+
     @JvmStatic
     fun main(args: Array<String>) {
         setup()
-        WikiApplication.startup()
+        startup()
     }
 
 }
